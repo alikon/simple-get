8efea6137ca49cf1ac3b30175612049537209658
alikon <optimus4joomla@gmail.com>
2021-10-27 21:39:38 +0200

    v

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,7 +8,7 @@ import { MessageService } from '../services/message.service';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messageServive: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }

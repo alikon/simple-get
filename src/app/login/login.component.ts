b09f4d070b56ede346bfb5b250aafc759c5c8d6c
Nicola Galgano <optimus4joomla@gmail.com>
2021-11-20 09:37:04 +0100

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { first } from 'rxjs/operators';
 import { AuthenticationService } from '../services/authentication.service';
 
 import { AlertService } from '../services/alert.service';
+import { AccountService } from '../services/account.service';
 
 const options = {
     autoClose: true,
@@ -14,60 +15,53 @@ const options = {
 
 @Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
+    form: FormGroup;
     loading = false;
     submitted = false;
-    returnUrl: string;
-    error = '';
+
 
     constructor(
         private formBuilder: FormBuilder,
         private route: ActivatedRoute,
         private router: Router,
-        private authenticationService: AuthenticationService,
-        public alertService: AlertService
-    ) {
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) {
-            this.router.navigate(['/']);
-        }
-    }
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
 
+    
     ngOnInit() {
-        this.loginForm = this.formBuilder.group({
+        this.form = this.formBuilder.group({
             username: ['', Validators.required],
             password: ['', Validators.required]
         });
-
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
+     // convenience getter for easy access to form fields
+     get f() { return this.form.controls; }
 
     onSubmit() {
         this.submitted = true;
 
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
+        // reset alerts on submit
+        this.alertService.clear();
 
+        // stop here if form is invalid
+        if (this.form.invalid) {
             return;
         }
 
         this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
+        this.accountService.login(this.f.username.value, this.f.password.value)
             .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                    this.alertService.success('Success!!', options);
+            .subscribe({
+                next: () => {
+                    // get return url from query parameters or default to home page
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    this.router.navigateByUrl(returnUrl);
                 },
-                error => {
-                    console.log(error.error.message);
-                    this.error = error.error.message;
+                error: error => {
+                    this.alertService.error(error);
                     this.loading = false;
-                    this.alertService.error(this.error, options);
-                });
-    }
-}
+                }
+            });
+    }}
